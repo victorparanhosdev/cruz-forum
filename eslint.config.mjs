5a960e9ac0bc3270ce55cf7eccda19803e4bf4d9
Victor Paranhos <victor.costa@sema.ba.gov.br>
2025-03-11 12:05:36 -0300

    fix:nova config do eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import typescriptEslint from '@typescript-eslint/eslint-plugin'
 import prettier from 'eslint-plugin-prettier'
 import tailwindcss from 'eslint-plugin-tailwindcss'
+import unusedImports from 'eslint-plugin-unused-imports'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import js from '@eslint/js'
@@ -27,12 +28,25 @@ export default [
       '@typescript-eslint': typescriptEslint,
       prettier,
       tailwindcss,
+      'unused-imports': unusedImports,
     },
 
     rules: {
       'prettier/prettier': 'error',
       'tailwindcss/classnames-order': 'warn',
       'tailwindcss/no-custom-classname': 'off',
+      'import/no-anonymous-default-export': 'off',
+      'no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
     },
   },
 ]
